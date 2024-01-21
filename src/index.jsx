1e24678edbe88e8a8c280b90c0f4f83aefc1196e
wangkodok <wangkodok@gmail.com>
2024-01-21 14:08:55 +0900

    styled-reset 설치

--- src/index.jsx
+++ src/index.jsx
@@ -1,4 +1,10 @@
 import ReactDOM from "react-dom/client";
+import { Reset } from "styled-reset";
 import App from "./App.jsx";
 
-ReactDOM.createRoot(document.getElementById("root")).render(<App />);
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <>
+    <Reset />
+    <App />
+  </>
+);
