1085bd346828091a8b1dfbb0de9feea8cbd31cf6
wangkodok <wangkodok@gmail.com>
2024-02-02 19:21:04 +0900

    삼항 연산자를 if문으로 변경

--- src/components/BirthDayList.jsx
+++ src/components/BirthDayList.jsx
@@ -3,15 +3,13 @@ import NoItemsMessage from "./NoItemsMessage.jsx";
 import { date } from "../date.js";
 
 export default function BirthDayList() {
-  return (
-    <ul className="birthday-list">
-      {date.length === 0 ? (
-        <NoItemsMessage innerText="생일을 추가하세요." />
-      ) : (
-        date.map((dates, index) => {
-          return <BirthDayItem key={index} dates={dates} />;
-        })
-      )}
-    </ul>
-  );
+  let itemResultGet = <NoItemsMessage innerText="생일을 추가하세요." />;
+
+  if (date.length !== 0) {
+    itemResultGet = date.map((dates, index) => {
+      return <BirthDayItem key={index} dates={dates} />;
+    });
+  }
+
+  return <ul className="birthday-list">{itemResultGet}</ul>;
 }
