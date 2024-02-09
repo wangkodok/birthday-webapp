73ab36baf77b14d1b840d6a29017e0308bcbc438
wangkodok <wangkodok@gmail.com>
2024-02-09 23:45:29 +0900

    하단 내비게이션 아이콘 추가

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,19 +6,19 @@ export default function Header() {
       <ul>
         <li>
           <Link to="/">
-            <div className="icon"></div>
+            <div className="icon birthday-cake"></div>
             <p className="icon-text">생일</p>
           </Link>
         </li>
         <li>
           <Link to="/calendar">
-            <div className="icon"></div>
+            <div className="icon calendar"></div>
             <p className="icon-text">달력</p>
           </Link>
         </li>
         <li>
           <Link to="/setting">
-            <div className="icon"></div>
+            <div className="icon settings"></div>
             <p className="icon-text">설정</p>
           </Link>
         </li>
