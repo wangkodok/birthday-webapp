4346d3b5f3acff4bafa0ae35453b6f711ffbc03d
wangkodok <wangkodok@gmail.com>
2024-02-15 18:13:44 +0900

    생일 날짜 컴포넌트 생성

---
+++ src/components/BirthdayDate.jsx
@@ -0,0 +1,65 @@
+/* eslint-disable */
+export default function BirthdayDate({ onHandleChange }) {
+  return (
+    <label htmlFor="month-day">
+      <select
+        name="month"
+        id="month"
+        defaultValue={"월"}
+        onChange={onHandleChange}
+      >
+        <option value="월" disabled>
+          월
+        </option>
+        <option value="1">1</option>
+        <option value="2">2</option>
+        <option value="3">3</option>
+        <option value="4">4</option>
+        <option value="5">5</option>
+        <option value="6">6</option>
+        <option value="7">7</option>
+        <option value="8">8</option>
+        <option value="9">9</option>
+        <option value="10">10</option>
+        <option value="11">11</option>
+        <option value="12">12</option>
+      </select>
+      <select name="day" id="day" defaultValue={"일"} onChange={onHandleChange}>
+        <option value="일" disabled>
+          일
+        </option>
+        <option value="1">1</option>
+        <option value="2">2</option>
+        <option value="3">3</option>
+        <option value="4">4</option>
+        <option value="5">5</option>
+        <option value="6">6</option>
+        <option value="7">7</option>
+        <option value="8">8</option>
+        <option value="9">9</option>
+        <option value="10">10</option>
+        <option value="11">11</option>
+        <option value="12">12</option>
+        <option value="13">13</option>
+        <option value="14">14</option>
+        <option value="15">15</option>
+        <option value="16">16</option>
+        <option value="17">17</option>
+        <option value="18">18</option>
+        <option value="19">19</option>
+        <option value="20">20</option>
+        <option value="21">21</option>
+        <option value="22">22</option>
+        <option value="23">23</option>
+        <option value="24">24</option>
+        <option value="25">25</option>
+        <option value="26">26</option>
+        <option value="27">27</option>
+        <option value="28">28</option>
+        <option value="29">29</option>
+        <option value="30">30</option>
+        <option value="31">31</option>
+      </select>
+    </label>
+  );
+}
