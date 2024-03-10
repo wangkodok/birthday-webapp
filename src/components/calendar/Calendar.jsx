a9eb37603878013682f4b2f0a697a6d1b47ed11e
wangkodok <wangkodok@gmail.com>
2024-03-10 22:11:35 +0900

    자식 컴포넌트 상태를 부모로 끌어올려 props로 전달

--- src/components/calendar/Calendar.jsx
+++ src/components/calendar/Calendar.jsx
@@ -1,103 +1,18 @@
 /* eslint-disable */
-import { useState } from "react";
 import "./Calendar.css";
 
 export default function Calendar({
-  setBirthdayListDate,
-  currentMonth,
-  setCurrentMonth,
-  date,
-  setResultDefault,
-  setDatePicker,
+  dayOfTheWeek,
+  year,
+  month,
+  day,
+  curMonthStartDate,
+  arr,
+  dayDateNext,
+  prevMonth,
+  nextMonth,
+  BirthdayList,
 }) {
-  const dayOfTheWeek = ["일", "월", "화", "수", "목", "금", "토"];
-  // const [date, setDate] = useState([]);
-
-  // 현재 몇 년, 몇 월
-  const year = currentMonth.getFullYear();
-  const month = currentMonth.getMonth() + 1;
-  const dd = currentMonth.getDate();
-
-  // 현재 월의 시작하는 요일
-  const curMonthStartDate = new Date(
-    currentMonth.getFullYear(), // 년도
-    currentMonth.getMonth() + 0, // 0는 1월, 1는 2월, 2는 3월 ··· 10는 11월, 11는 12월
-    1 // 현재 월 시작하는 날짜를 반환
-  ).getDay();
-
-  // 현재 월의 마지막 날짜
-  const curMonthEndDate = new Date(
-    currentMonth.getFullYear(), // 년도
-    currentMonth.getMonth() + 1,
-    0 // 전날 월의 마지막 날짜를 반환
-  ).getDate();
-
-  // 현재 월의 모든 날짜
-  const arr = Array.from(Array(curMonthEndDate), (_, index) => index + 1);
-
-  // 이전 월의 마지막 날짜
-  const prev = new Date(
-    currentMonth.getFullYear(), // 년도
-    currentMonth.getMonth(),
-    0 // 전날 월의 마지막 날짜를 반환
-  ).getDate();
-
-  // 월의 첫째 주 없는 날짜
-  const dayDatePrev = [];
-  for (let index = prev; index > prev - curMonthStartDate; index--) {
-    dayDatePrev.push(index);
-  }
-  arr.unshift(...dayDatePrev.reverse());
-
-  // 다음 월의 시작하는 요일
-  const next = new Date(
-    currentMonth.getFullYear(),
-    currentMonth.getMonth() + 1,
-    1
-  ).getDay();
-
-  // 월의 마지막 주 없는 날짜
-  const dayDateNext = [];
-  for (let index = 0; index < 7 - next; index++) {
-    dayDateNext.push(index + 1);
-  }
-  arr.push(...dayDateNext);
-
-  // 이전 달
-  function prevMonth() {
-    setCurrentMonth(
-      new Date(
-        currentMonth.getFullYear(),
-        currentMonth.getMonth() - 1,
-        currentMonth.getDate()
-      )
-    );
-  }
-
-  // 다음 달
-  function nextMonth() {
-    setCurrentMonth(
-      new Date(
-        currentMonth.getFullYear(),
-        currentMonth.getMonth() + 1,
-        currentMonth.getDate()
-      )
-    );
-  }
-
-  // 해당하는 날짜의 생일 목록
-  function BirthdayList(e) {
-    const dateResult = date.filter((item) => {
-      if (parseInt(item.month) === month && item.day === e.target.innerText) {
-        return true;
-      }
-    });
-
-    setBirthdayListDate(dateResult);
-    setResultDefault(true);
-    setDatePicker(e.target.innerText);
-  }
-
   return (
     <>
       <div className="year-month">
@@ -115,7 +30,7 @@ export default function Calendar({
       <ul className="few-days">
         {arr.map((item, index) => {
           for (let i = 0; i < arr.length; i++) {
-            if (curMonthStartDate + dd - 1 === index) {
+            if (curMonthStartDate + day - 1 === index) {
               return (
                 <li className="current-month day" key={index}>
                   <button onClick={BirthdayList}>{item}</button>
--- src/pages/CalendarPage.jsx
+++ src/pages/CalendarPage.jsx
@@ -1,14 +1,112 @@
-import Wrapper from "../components/Wrapper";
-import Calendar from "../components/calendar/Calendar";
-// import BirthDayList from "../components/BirthDayList";
 import { useState } from "react";
 import { date } from "../date";
 
+import Wrapper from "../components/Wrapper";
+import Calendar from "../components/calendar/Calendar";
+
 export default function CalendarPage() {
-  const [birthdayListDate, setBirthdayListDate] = useState([]);
-  const [currentMonth, setCurrentMonth] = useState(new Date());
-  const [resultDefault, setResultDefault] = useState();
-  const [datePicker, setDatePicker] = useState();
+  const [dates, setDates] = useState({
+    birthdayListDate: [],
+    currentMonth: new Date(),
+  });
+
+  const dayOfTheWeek = ["일", "월", "화", "수", "목", "금", "토"];
+
+  // 현재 몇 년, 몇 월
+  const yyyy = dates.currentMonth.getFullYear();
+  const mm = dates.currentMonth.getMonth() + 1;
+  const dd = dates.currentMonth.getDate();
+
+  // 현재 월의 시작하는 요일
+  const curMonthStartDate = new Date(
+    dates.currentMonth.getFullYear(), // 년도
+    dates.currentMonth.getMonth() + 0, // 0는 1월, 1는 2월, 2는 3월 ··· 10는 11월, 11는 12월
+    1 // 현재 월 시작하는 날짜를 반환
+  ).getDay();
+
+  // 현재 월의 마지막 날짜
+  const curMonthEndDate = new Date(
+    dates.currentMonth.getFullYear(), // 년도
+    dates.currentMonth.getMonth() + 1,
+    0 // 전날 월의 마지막 날짜를 반환
+  ).getDate();
+
+  // 현재 월의 모든 날짜
+  const arr = Array.from(Array(curMonthEndDate), (_, index) => index + 1);
+
+  // 이전 월의 마지막 날짜
+  const prev = new Date(
+    dates.currentMonth.getFullYear(), // 년도
+    dates.currentMonth.getMonth(),
+    0 // 전날 월의 마지막 날짜를 반환
+  ).getDate();
+
+  // 월의 첫째 주 없는 날짜
+  const dayDatePrev = [];
+  for (let index = prev; index > prev - curMonthStartDate; index--) {
+    dayDatePrev.push(index);
+  }
+  arr.unshift(...dayDatePrev.reverse());
+
+  // 다음 월의 시작하는 요일
+  const next = new Date(
+    dates.currentMonth.getFullYear(),
+    dates.currentMonth.getMonth() + 1,
+    1
+  ).getDay();
+
+  // 월의 마지막 주 없는 날짜
+  const dayDateNext = [];
+  for (let index = 0; index < 7 - next; index++) {
+    dayDateNext.push(index + 1);
+  }
+  arr.push(...dayDateNext);
+
+  // 이전 달
+  function prevMonth() {
+    setDates((prevState) => {
+      return {
+        ...prevState,
+        currentMonth: new Date(
+          prevState.currentMonth.getFullYear(),
+          prevState.currentMonth.getMonth() - 1,
+          prevState.currentMonth.getDate()
+        ),
+      };
+    });
+  }
+
+  // 다음 달
+  function nextMonth() {
+    setDates((prevState) => {
+      return {
+        ...prevState,
+        currentMonth: new Date(
+          prevState.currentMonth.getFullYear(),
+          prevState.currentMonth.getMonth() + 1,
+          prevState.currentMonth.getDate()
+        ),
+      };
+    });
+  }
+
+  // 해당하는 날짜의 생일 목록
+  function BirthdayList(e) {
+    const dateResult = date.filter((item) => {
+      if (parseInt(item.month) === mm && item.day === e.target.innerText) {
+        return true;
+      }
+    });
+
+    setDates((prevState) => {
+      return {
+        ...prevState,
+        birthdayListDate: dateResult,
+        resultDefault: true,
+        datePicker: e.target.innerText,
+      };
+    });
+  }
 
   return (
     <Wrapper>
@@ -16,23 +114,29 @@ export default function CalendarPage() {
         <section className="sec-calendar">
           <div className="calendar-area">
             <Calendar
-              setBirthdayListDate={setBirthdayListDate}
-              currentMonth={currentMonth}
-              setCurrentMonth={setCurrentMonth}
-              date={date}
-              setResultDefault={setResultDefault}
-              setDatePicker={setDatePicker}
+              dayOfTheWeek={dayOfTheWeek}
+              year={yyyy}
+              month={mm}
+              day={dd}
+              curMonthStartDate={curMonthStartDate}
+              arr={arr}
+              dayDateNext={dayDateNext}
+              prevMonth={prevMonth}
+              nextMonth={nextMonth}
+              BirthdayList={BirthdayList}
             />
           </div>
           <div className="calendar-list">
             {/* <p>2024년 12월 31일</p> */}
-            {birthdayListDate.length === 0 ? <p>생일자가 없습니다.</p> : null}
+            {dates.birthdayListDate.length === 0 ? (
+              <p>생일자가 없습니다.</p>
+            ) : null}
             <ul className="birthday-list">
               {date.map((item, key) => {
-                if (resultDefault === undefined) {
+                if (dates.resultDefault === undefined) {
                   if (
-                    item.month == currentMonth.getMonth() + 1 &&
-                    item.day == currentMonth.getDate()
+                    item.month == dates.currentMonth.getMonth() + 1 &&
+                    item.day == dates.currentMonth.getDate()
                   ) {
                     return (
                       <li key={key}>
@@ -52,10 +156,10 @@ export default function CalendarPage() {
               })}
             </ul>
             <ul className="birthday-list">
-              {birthdayListDate.map((item, key) => {
+              {dates.birthdayListDate.map((item, key) => {
                 if (
-                  item.month == currentMonth.getMonth() + 1 &&
-                  item.day == datePicker
+                  item.month == dates.currentMonth.getMonth() + 1 &&
+                  item.day == dates.datePicker
                 ) {
                   return (
                     <li key={key}>
@@ -73,16 +177,6 @@ export default function CalendarPage() {
                 }
               })}
             </ul>
-            <div>
-              {/* {birthdayListDate.map((item, key) => {
-                return (
-                  <div key={key}>{item.name} "클릭하면 나오는 목록들"</div>
-                );
-              })} */}
-            </div>
-            {/* {birthdayListDate.length === 0 ? null : (
-              <BirthDayList birthdayListDate={birthdayListDate} />
-            )} */}
           </div>
         </section>
       </main>
