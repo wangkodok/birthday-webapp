7dd76df42c72ed6ba1451cd6979694a3fe1389b5
wangkodok <wangkodok@gmail.com>
2024-03-21 18:04:15 +0900

    생일 목록 레이아웃에 태그 추가 및 CSS 작업

--- src/components/BirthDayItem.jsx
+++ src/components/BirthDayItem.jsx
@@ -4,18 +4,20 @@ import profileImg from "../assets/profile.png";
 export default function BirthDayItem({ dates }) {
   return (
     <li>
-      <div className="profile">
-        {dates.profile === "" ? (
-          <img src={profileImg} alt="" />
-        ) : (
-          <img src={dates.profile} alt="" />
-        )}
-      </div>
-      <div className="text">
-        <h3>{dates.name}</h3>
-        <p>
-          {dates.month}월 {dates.day}일
-        </p>
+      <div style={{ display: "flex", alignItems: "center" }}>
+        <div className="profile">
+          {dates.profile === "" ? (
+            <img src={profileImg} alt="" />
+          ) : (
+            <img src={dates.profile} alt="" />
+          )}
+        </div>
+        <div className="text">
+          <h3>{dates.name}</h3>
+          <p>
+            {dates.month}월 {dates.day}일
+          </p>
+        </div>
       </div>
       <div className="d-day">
         <span className="number">0</span>
